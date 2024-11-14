bdff9c04b95be5f19082022417471353b6d89c8a
lightningspeedd221 <jorttelussa0@gmail.com>
2024-11-14 18:30:18 +0100

    yurrr

---
+++ README.md
@@ -0,0 +1,2 @@
+First Website Ever Created By Me.
+Website: https://lightningspeedd221.github.io/First-Page/
---
+++ script.js
@@ -0,0 +1,51 @@
+
+function Refresh()
+{
+    location.reload();
+}
+
+function Pages(pagenbr)
+{
+    if(pagenbr == 1)
+    {
+        window.location.href = "index.html";
+    }
+    else if(pagenbr == 2)
+    {
+        window.location.href = "about.html";
+    }
+    else if(pagenbr == 3)
+    {
+        window.location.href = "spam.html";
+    }
+    else
+    {
+        alert("Unknown page");
+    }
+}
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+//* Hide loading screen when page is loaded
+window.addEventListener("load", async function() {
+    await delay(1000);
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    Video();
+});//
+
+function Video()
+{
+    if(document.getElementById("content").style.display == "block")
+    {
+        const video = document.getElementById('videoBackground');
+        video.style.display = 'block';  // Show the video
+        // Create an Audio object
+        const sound = new Audio('sounds/kingvon.mp3');
+        sound.volume = 0.5;
+        // Play the sound
+        video.play();                   // Play the video
+        sound.play();
+    }
+
+}
